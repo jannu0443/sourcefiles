4ef980c17863035eacdac0d311c2e29c9949f143
jannu0443 <141567203+jannu0443@users.noreply.github.com>
2023-08-10 17:13:27 +0530

    Add files via upload

---
+++ Armstrong.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+unsigned int n = 0,rem = 0,Sum = 0,temp = 0;
+
+printf("enter an integer:");
+scanf("%d",&n);
+
+temp=n;
+
+while(n!=0)
+{
+    rem=n%10;
+    Sum+=(rem*rem*rem);
+    n=n/10;
+}
+
+if(temp==Sum)
+printf("armstrong number \n");
+else
+printf("not an armstrong number");
+
+}
