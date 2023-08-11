96ac3d5132b1c7178a4a9d2af33c872e4d5ca60a
jannu0443 <141567203+jannu0443@users.noreply.github.com>
2023-08-11 14:45:32 +0530

    Add files via upload

---
+++ If_else_switch.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+int main()
+{
+unsigned int selection = 0;
+
+printf("Enter which statement need to execute:\n");
+scanf("%d",&selection);
+
+
+
+/* if else blocks*/
+
+/*if (selection == 1)
+    printf("first if statement\n");
+else if (selection == 2)
+    printf("second else if statement\n");
+else
+    printf("final else statement\n");*/
+
+
+/*Loops while,do while and for loop*/
+
+/*while(1)//selection++ <= 10)
+{
+    printf("while Selection value = %d\n",selection);
+}
+
+do
+{
+    printf("do while  Selection value = %d\n",selection);
+}while(selection++ <= 10);*/
+
+/*for loop & Nested for loop*/
+
+/*for(selection = 0;selection <=10;selection++)
+printf("Selection value = %d\n",selection);*/
+
+/*switch(selection)
+{
+    case 0 : printf("case 0 is executing\n");
+    break;
+    case 1 : printf("case 1 statement\n");
+    break;
+
+    case 99 : printf("case 99 is executing\n");
+    break;
+
+    default : printf("default case\n");
+    break;
+}*/
+
+
+
+
+}
